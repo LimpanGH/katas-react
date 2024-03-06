cab0969456686fe9caa4c01702f40f86e180b931
LimpanGH <Linusj555@gmail.com>
2024-03-06 11:14:22 +0100

    added stuff

---
+++ src/kataEtt.tsx
@@ -0,0 +1,63 @@
+
+// ! Info  ---------------------------------------------------------------------
+// * React.js is a JavaScript library developed by engineers at Facebook. Here are just a few of the reasons why people choose to program with React:
+// * React is fast. Apps made in React can handle complex updates and still feel quick and responsive.
+// * React is modular. Instead of writing large, dense files of code, you can write many smaller, reusable files. React’s modularity can be a beautiful solution to JavaScript’s maintainability problems.
+// * React is scalable. Large programs that display a lot of changing data are where React performs best.
+// * React is flexible. You can use React for interesting projects that have nothing to do with making a web app. People are still figuring out React’s potential. There’s room to explore.
+// * JSX is a syntax extension for JavaScript. It was written to be used with React. JSX code looks a lot like HTML.
+// * If a JavaScript file contains JSX code, then that file will have to be compiled. This means that before the file reaches a web browser, a JSX compiler will translate any JSX into regular JavaScript.
+// * A basic unit of JSX is called a JSX element. Here’s an example of a JSX element: <h1>Hello world</h1>
+// * JSX elements are treated as JavaScript expressions. They can go anywhere that JavaScript expressions can go. This means that a JSX element can be saved in a variable, passed to a function, stored in an object or array… you name it.
+
+// * Here’s an example of a JSX element being saved in a variable:
+// *   const navBar = <nav>I am a nav bar</nav>;
+
+// * Here’s an example of several JSX elements being stored in an object:
+// *   const myTeam = {
+// *     center: <li>Benzo Walli</li>,
+// *     powerForward: <li>Rasha Loa</li>,
+// *     smallForward: <li>Tayshaun Dasmoto</li>,
+// *     shootingGuard: <li>Colmar Cumberbatch</li>,
+// *     pointGuard: <li>Femi Billon</li>
+// *   };
+
+// * JSX elements can have attributes, just like how HTML elements can:
+// *  const panda = <img src='images/panda.jpg' alt='panda' width='500px' height='500px'>;
+
+// * NESTING. You can nest JSX elements inside of other JSX elements, just like in HTML.
+// * If a JSX expression takes up more than one line, then you must wrap the multi-line JSX expression in parentheses.
+// * Nested JSX expressions can be saved as variables, passed to functions, etc., just like non-nested JSX expressions can:
+// *    const theExample = (
+// *      <a href="https://www.example.com">
+// *        <h1>
+// *          Click me!
+// *        </h1>
+// *      </a>
+// *    );
+
+//* JSX expression must have exactly one outermost element, the solution is usually simple: wrap the JSX expression in a <div> element or a Fragment.
+
+// * Tailwind är vanligt i React
+
+// todo Props är imutable, one-way dataflow. Man kan alltså bara skicka props från parent till child och inte tvärtom!! Det gör att man får bättre koll på datan och det minskar buggar.
+
+// * React-komponent under huven är en funktion som gör saker tex renderar ut saker i DOMen
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// ! Small excercises ---------------------------------------------------------
+
+// * 1. Render 'Hello World' in the DOM
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('container');
+const root = createRoot(container);
+root.render(<h1>Hello world</h1>);
+
+// * 2. Passing a Variable to render()
+
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// Rör inte denna fil, den brukar stå såhär i original
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
